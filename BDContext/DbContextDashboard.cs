4c7dd4515a5bb1a2513570fa52eea6db3d6d88f8
Calos01 <ca_los01@hotmail.com>
2023-07-09 21:53:31 -0500

    dbcontext

---
+++ BDContext/DbContextDashboard.cs
@@ -0,0 +1,15 @@
+﻿using DashboardApiBack.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DashboardApiBack.BDContext
+{
+    public class DbContextDashboard:DbContext
+    {
+        public DbContextDashboard(DbContextOptions<DbContextDashboard> options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Server> Servers { get; set; }
+    }
+}
