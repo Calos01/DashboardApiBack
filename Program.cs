9e79da314c9aa33338d9282b2f59cbaf9d6898eb
Calos01 <ca_los01@hotmail.com>
2023-07-10 19:57:44 -0500

    conexionBd

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using DashboardApiBack.BDContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//conexion
+builder.Services.AddDbContext<DbContextDashboard>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("Conexion")));
 
 var app = builder.Build();
 
