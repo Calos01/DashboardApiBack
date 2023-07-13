c4b57b840473b3e36037f27b4d3e6c2e0186d23d
Calos01 <ca_los01@hotmail.com>
2023-07-12 20:15:17 -0500

    controllerCustomer

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,43 @@
+﻿using DashboardApiBack.BDContext;
+using DashboardApiBack.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DashboardApiBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly DbContextDashboard _context;
+        public CustomerController(DbContextDashboard context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult GetCustomer() { 
+            var result= _context.Customers.ToList();
+            return Ok(result);
+        }
+        [HttpGet("{id}")]
+        public ActionResult GetCustomerForId(int id)
+        {
+            var result = _context.Customers.Find(id);  
+            return Ok(result);
+        }
+
+        [HttpPost("Añadir")]
+        public ActionResult PostCustomer(Customer customer) { 
+            if (customer == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                _context.Customers.Add(customer);
+                _context.SaveChanges();
+                return Ok("agregado");
+            }
+        }
+    }
+}
